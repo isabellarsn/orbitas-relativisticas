eda848a8675a54aa9068ea4fbd25e70db875c5fd
isabellarsn <98843987+isabellarsn@users.noreply.github.com>
2022-03-08 00:56:40 -0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -35,7 +35,7 @@ if pagina_selecionada == "Óbita de corpos celestes":
     st.title("Corpo celeste orbitando um buraco negro")
     st.write("E se, de repente, o Sol se transformasse em um buraco negro?")
     st.write("Para isso, toda sua massa, de 2 $\cdot$ 10$^{30}$ kg (hoje espalhada numa esfera com cerca de  700.000 km de raio), deveria ser comprimida numa região com raio de cerca de  3  km.")
-    image = Image.open(r'C:/Users/isabe/.streamlit/orbitaceleste.png')
+    image = Image.open('orbitaceleste.png')
     st.image(image)
     st.write("O protótipo de simulador abaixo nos permite explorar a órbita de corpos (planetas, asteroides ou espaço-naves) que se aventurassem nas vizinhanças de um buraco negro com a mesma massa do Sol.")
     st.write("Nele você pode alterar (ver figura acima):")
@@ -239,7 +239,7 @@ if pagina_selecionada == "Óbita de corpos celestes":
 elif pagina_selecionada == "Órbita de raios de luz":
     st.title("Luz orbitando um buraco negro")
     st.write("A teoria da relatividade geral de Einstein prevê que a trajetória da luz deve ser defletida quando passa nas vizinhanças de um corpo massivo. O protótipo de simulador abaixo nos permite explorar a órbita raios de luz ao redor de um buraco negro com a mesma massa do Sol.")
-    image = Image.open(r'C:/Users/isabe/.streamlit/orbitaluz.png')
+    image = Image.open('orbitaluz.png')
     st.image(image)
     st.write("Nele, você pode alterar (ver figura acima):")
     st.write("* O parâmetro de impacto (em km): $d$")
